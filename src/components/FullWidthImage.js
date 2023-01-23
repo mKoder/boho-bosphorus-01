a23befee8400bd6a6a9308f39de70419dd3ebea6
Edward Lynn (DevHaus) <edward.lynn@devhaus.co.uk>
2023-01-23 23:58:41 +0300

    Change title position

--- src/components/FullWidthImage.js
+++ src/components/FullWidthImage.js
@@ -18,10 +18,57 @@ export default function FullWidthImage(props) {
       <div
         className="margin-top-0"
         style={{
-          display: "grid",
           alignItems: "center",
         }}
       >
+        {(title || subheading) && (
+          <div
+            style={{
+              // By using the same grid area for both, they are stacked on top of each other
+              // gridArea: "1/1",
+              // position: "absolute",
+              // This centers the other elements inside the hero component
+              // placeItems: "start center",
+              // display: "grid",
+            }}
+          >
+            {/* Any content here will be centered in the component */}
+            {title && (
+              <h1
+                className={h1Class}
+                style={{
+                  backgroundColor: "rgb(28, 27, 26, 0.9)",
+                  color: "#dedede",
+                  lineHeight: "1",
+                  padding: "0.25em 1.25em 0.25em 1.25em",
+                  // transform: `${h1Transform}`,
+                  "z-index": "31",
+                  transform: "rotate(-90deg) translate(-100%, 0)",
+                  "transform-origin": "left top",
+                  position: "absolute"
+                }}
+              >
+                {title}
+              </h1>
+            )}
+            {subheading && (
+              <h3
+                className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
+                style={{
+                  boxShadow:
+                    "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
+                  backgroundColor: "rgb(255, 68, 0)",
+                  color: "white",
+                  lineHeight: "1",
+                  padding: "0.25rem",
+                  marginTop: "0.5rem",
+                }}
+              >
+                {subheading}
+              </h3>
+            )}
+          </div>
+        )}
         {img?.url ? (
           <img
             src={img}
@@ -53,51 +100,6 @@ export default function FullWidthImage(props) {
             alt=""
             formats={["auto", "webp", "avif"]}
           />
-        )}
-        {(title || subheading) && (
-          <div
-            style={{
-              // By using the same grid area for both, they are stacked on top of each other
-              gridArea: "1/1",
-              position: "absolute",
-              // This centers the other elements inside the hero component
-              placeItems: "start center",
-              display: "grid",
-            }}
-          >
-            {/* Any content here will be centered in the component */}
-            {title && (
-              <h1
-                className={h1Class}
-                style={{
-                  backgroundColor: "rgb(28, 27, 26, 0.9)",
-                  color: "#dedede",
-                  lineHeight: "1",
-                  padding: "0.25em 1.25em 0.25em 1.25em",
-                  transform: `${h1Transform}`,
-                  "z-index": "31",
-                }}
-              >
-                {title}
-              </h1>
-            )}
-            {subheading && (
-              <h3
-                className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-                style={{
-                  boxShadow:
-                    "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
-                  backgroundColor: "rgb(255, 68, 0)",
-                  color: "white",
-                  lineHeight: "1",
-                  padding: "0.25rem",
-                  marginTop: "0.5rem",
-                }}
-              >
-                {subheading}
-              </h3>
-            )}
-          </div>
         )}
       </div>
     </React.Fragment>
