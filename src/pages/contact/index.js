3eeab8b06e3fd50936e332e389b9abcd45be86ba
Edward Lynn <edward.lynn@avios.com>
2024-06-16 18:14:36 +0100

    Contact us style tweak

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -11,15 +11,16 @@ export default () => {
                 <div className="columns">
                   <div className="column is-12 m-auto">
                     <div className="column is-8 m-auto container">
-                      <h2 className="has-text-weight-bold is-size-2">Contacting us</h2>
-                      <section className="section">
-                        <div className="block">You can contact us on <a href="tel:+90 537 810 2705">+90 537 810 2705</a> or email us at
-                          &nbsp;<a href="mailto:bohobosphorus@gmail.com">bohobosphorus@gmail.com</a>
-                        </div>
-                        <div className="block">To inquire a quote for your event please fill out the&nbsp;
-                          <a href="https://bonjukbay.plumsail.io/6419b524-873b-4f70-9efc-601e7111a644">Request form</a>
-                        </div>
-                      </section>
+                      <div className="block">
+                        <h2 className="has-text-weight-bold is-size-2">Contacting us</h2>
+                      </div>
+                      <div className="block">You can contact us on <a href="tel:+90 537 810 2705">+90 537 810 2705</a> or email us at
+                        &nbsp;<a href="mailto:bohobosphorus@gmail.com">bohobosphorus@gmail.com</a>
+                      </div>
+                      <div className="block">To inquire a quote for your event please fill out the&nbsp;
+                        <a href="https://bonjukbay.plumsail.io/6419b524-873b-4f70-9efc-601e7111a644">Request form</a>
+                      </div>
+
                     </div>
                   </div>
                 </div>
